66c2cd9a0481f51d5d8c755427acfa3ee07077a1
kabowyad <kabowyad@gmail.com>
2019-04-11 12:52:30 +0500

    научился писать в csv

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,19 +1,22 @@
+import java.io.File
+
 import kantan.csv._
 import kantan.csv.ops._
+import kantan.csv.generic._
 
 case class Heart(age: Int, sex: Int, cp: Int, trestbps: Int, chol: Int, fbs: Int,
-                   restecg: Int, thalach: Int, exang: Int, oldpeak: Float, slope: Int,
-                   ca: Int, thal: Int, target: Int)
+                 restecg: Int, thalach: Int, exang: Int, oldpeak: Double, slope: Int,
+                 ca: Int, thal: Int, target: Int)
 
 
 object Main extends App {
   implicit val heartDecoder: RowDecoder[Heart] = RowDecoder.ordered {
     (age: Int, sex: Int, cp: Int, trestbps: Int, chol: Int, fbs: Int,
-     restecg: Int, thalach: Int, exang: Int, oldpeak: Float, slope: Int,
+     restecg: Int, thalach: Int, exang: Int, oldpeak: Double, slope: Int,
      ca: Int, thal: Int, target: Int) =>
 
       new Heart(age: Int, sex: Int, cp: Int, trestbps: Int, chol: Int, fbs: Int,
-        restecg: Int, thalach: Int, exang: Int, oldpeak: Float, slope: Int,
+        restecg: Int, thalach: Int, exang: Int, oldpeak: Double, slope: Int,
         ca: Int, thal: Int, target: Int)
   }
   val pathToFile = "/titanic.csv"
@@ -21,7 +24,12 @@ object Main extends App {
 
   def processCsv(pathToFile: String) = {
     val rawData: java.net.URL = getClass.getResource(pathToFile)
-    val list = rawData.asCsvReader[Heart](rfc.withHeader).toList
-    list
+    val rawList = rawData.readCsv[List, Heart](rfc.withHeader)
+    val list = rawList.map(x => x.getOrElse(Heart(0,0,0,0,0,0,0,0,0,0.0,0,0,0,0)))
+    list.foreach(println _)
+    val out = new File("kantan.csv")
+    val writer = out.asCsvWriter[Heart](rfc.withHeader("Age", "Sex", "Cp", "Trestbps", "Chol", "fbs", "restecg", "thalach", "exang",
+      "oldpeak", "slope", "ca", "thal", "target"))
+    writer.write(list).close()
   }
 }
