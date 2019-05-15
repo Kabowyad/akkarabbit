f2f8aa87e2b0541720ba18ab2295c80dab9f13d3
yan <kabowyad@gmail.com>
2019-05-15 19:08:50 +0500

    Добавил: 1) Фильтрацию по 3 полям 2) Отрезаем по кол-ву записей

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -20,19 +20,29 @@ object Main extends App {
         ca: Int, thal: Int, target: Int)
   }
   val pathToFile = "/titanic.csv"
-  val csv = processCsv(pathToFile)
+  override def main(args: Array[String]): Unit = {
+    val fieldName = args(0)
+    val count = args(1)
+    processCsv(pathToFile, fieldName, count.toInt)
+  }
 
-  def processCsv(pathToFile: String) = {
-    val rawData: java.net.URL = getClass.getResource(pathToFile)
-    val rawList = rawData.readCsv[List, Heart](rfc.withHeader)
-    val list = rawList.map(x => x.getOrElse(Heart(0,0,0,0,0,0,0,0,0,0.0,0,0,0,0)))
-    writeToCsv(list)
+  def processCsv(pathToFile: String, fieldName: String, count: Int) = {
+    var rawData: java.net.URL = getClass.getResource(pathToFile)
+    var rawList = rawData.readCsv[List, Heart](rfc.withHeader)
+    var list = rawList.map(x => x.getOrElse(Heart(0,0,0,0,0,0,0,0,0,0.0,0,0,0,0)))
+    var finalList = fieldName match {
+      case "age" => list.sortWith(_.age > _.age)
+      case "sex" => list.sortWith(_.sex > _.sex)
+      case "cp" => list.sortWith(_.cp > _.cp)
+      case "oldpeak" => list.sortWith(_.oldpeak > _.oldpeak)
+    }
+    writeToCsv(finalList.take(count))
   }
 
   def writeToCsv(list: List[Heart]): Unit = {
     val out = new File("kantan.csv")
-    val writer = out.asCsvWriter[Heart](rfc.withHeader("Age", "Sex", "Cp", "Trestbps", "Chol", "fbs", "restecg", "thalach", "exang",
-      "oldpeak", "slope", "ca", "thal", "target"))
+    val writer = out.asCsvWriter[Heart](rfc.withHeader("Age", "Sex", "Cp"))
     writer.write(list).close()
+    println(out.toPath.toAbsolutePath)
   }
 }
